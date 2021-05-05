586d0f427be6a3222b0408b3a9f9737bab978730
brNakano <bruno.nakanoo@gmail.com>
2021-05-05 00:16:07 -0300

    navigationdrawer working-need position menu icon

--- app/src/main/java/com/example/sossalao/HomeActivity.kt
+++ app/src/main/java/com/example/sossalao/HomeActivity.kt
@@ -1,19 +1,25 @@
 package com.example.sossalao
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.SystemClock
+import android.view.MenuItem
 import android.view.View
+import android.widget.RelativeLayout
 import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.core.view.marginLeft
+import com.google.android.material.navigation.NavigationView
 import kotlinx.android.synthetic.main.activity_home.*
+import kotlinx.android.synthetic.main.bottombar.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 
-class HomeActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
@@ -29,11 +35,61 @@ class HomeActivity : AppCompatActivity() {
                 R.id.action_search -> Toast.makeText(this@HomeActivity,"Deseja pesquisar algo?", Toast.LENGTH_SHORT).show()
                 R.id.action_config -> startActivity(Intent(this, SettingsActivity::class.java))
                 R.id.action_refresh -> loading()
-                R.id.action_logout -> startActivity(Intent(this, LoginActivity::class.java))
+                R.id.action_logout -> finish()
+                R.id.menu_toggle -> layoutMenuLateral.openDrawer(GravityCompat.START)
+
             };   true
 
         }
 
+        configuraMenuLateral()
+
+    }
+
+    // adiciona os atributos da view
+    private fun configuraMenuLateral() {
+        var toogle = ActionBarDrawerToggle(
+            this,
+            layoutMenuLateral,
+            R.string.nav_open,
+            R.string.nav_close)
+
+        layoutMenuLateral.addDrawerListener(toogle)
+        toogle.syncState()
+
+        menu_lateral.setNavigationItemSelectedListener(this)
+    }
+
+
+    // tabs do menu lateral
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.scheduling -> {
+                Toast.makeText(this, "Clicou agenda", Toast.LENGTH_SHORT).show()
+            }
+
+            R.id.user_management -> {
+                Toast.makeText(this, "Clicou Usuários", Toast.LENGTH_SHORT).show()
+            }
+
+            R.id.clients -> {
+                Toast.makeText(this, "Clicou Clientes", Toast.LENGTH_SHORT).show()
+            }
+
+            R.id.products -> {
+                Toast.makeText(this, "Clicou Produtos", Toast.LENGTH_SHORT).show()
+            }
+
+            R.id.procedure -> {
+                Toast.makeText(this, "Clicou procedure", Toast.LENGTH_SHORT).show()
+            }
+            R.id.lateral_logout -> {
+                finish()
+            }
+        }
+
+        layoutMenuLateral.closeDrawer(GravityCompat.START)
+        return true
     }
 
     private fun loading(){
